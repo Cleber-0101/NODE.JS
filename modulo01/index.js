b6eef18b65bd2680a9a4268c7f797fe6d088a9bf
Cleber-0101 <cleberbatista99@hotmail.com>
2024-12-23 15:19:56 -0300

    Usando Middleware na minha API

--- modulo01/index.js
+++ modulo01/index.js
@@ -9,15 +9,62 @@ server.use(express.json());
 
 //CRUD >  CREATE ,READ , UPDATE , DELETE
 
-const cursos = ['Node.js' , 'javascript' , 'reactnative' ,'cleber'];
+const cursos = ['Node.js' , 'javascript' , 'reactnative' ,'Cleber'];
 
 //Read - buscando todos os cursos
 server.get('/cursos' , (req,res)=> {
   return res.json(cursos);
 })
 
+//Middleware Global 
+server.use((req,res,next)=>{
+  console.log(`URL CHAMADA ${req.url}`)
+
+  return next();
+})
+ 
+//middleware 
+function verificandoSeEstaPassandoNomeDosCursos(req, res, next){
+  if(!req.body.name){
+    return res.status(400).json({error: "Nome do curso é obrigatorio!!!"})
+  }
+
+  return next();
+}
+
+//middleware 
+function verificandoUrlExistente(req, res, next){
+  const curso = cursos[req.params.index]
+  if(!curso){
+    return res.status(400).json({
+      error: "O curso não existe"
+    })
+  }
+
+  req.curso = curso
+
+  return next();
+}
+
+// function cursoCriadoComSucesso(req, res, next) {
+//   if (req.body.name) {
+//     return res.status(200).json({ mensagem: "Curso criado com Sucesso" });
+//   }
+
+//   return next();
+// }
+
+
+
+//Get - Busca um usuario via indexados(url-id)
+server.get('/cursos/:index' , verificandoUrlExistente, (req , res) => {
+  const {index} = req.params;
+
+  return res.json(req.curso);
+})
+
 //Post - criando novo curso
-server.post('/cursos' , (req,res)=> {
+server.post('/cursos', verificandoSeEstaPassandoNomeDosCursos,(req,res)=> {
   const {name} = req.body;
   cursos.push(name);
 
@@ -25,7 +72,7 @@ server.post('/cursos' , (req,res)=> {
 })
 
 //Put - Atualizando curso 
-server.put('/cursos/:index', (req ,res)=> {
+server.put('/cursos/:index', verificandoSeEstaPassandoNomeDosCursos, verificandoUrlExistente, (req ,res)=> {
   const {index} = req.params;
   const {name} = req.body
   cursos[index] = name;
@@ -33,22 +80,11 @@ server.put('/cursos/:index', (req ,res)=> {
   return res.json(cursos)
 })
 
-// localhost:3000/curso
-server.get('/cursos/:index' , (req , res) => {
-  //PERGUNTANDO , BUSCANDO REQUERENDO
-  const {index} = req.params;
-
-  //REPOSTA DA REQUISIÇÃO , DEVOLVENDO RESPONSE
-  return res.json(cursos[index]);
-})
-
 //Excluindo curso 
-server.delete('/cursos/:index', (req,res) => {
+server.delete('/cursos/:index', verificandoUrlExistente, (req,res) => {
   const {index} = req.params
   cursos.splice(index , 1)
   return res.json({ mensagem : "Curso deletado com sucesso" })
 })
-
-
 
 server.listen(3000);
